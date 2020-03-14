119a37acb4fe0c16f49e22e81c8cbf4a1bd084e9
Patrick Vestal <patrick.vestal@gmail.com>
2020-03-14 11:34:39 -0400

    fb config

---
+++ src/components/Calendar.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="calendar">
+    <template>
+      <FullCalendar
+        defaultView="timeGridWeek"
+        :plugins="calendarPlugins"
+        :events="calendarEvents"
+        :header="{
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+        }"
+        :weekends="calendarWeekends"
+        :slotDuration="'00:30:00'"
+        :minTime="'09:00:00'"
+        :maxTime="'17:00:00'"
+        @dateClick="handleDateClick"
+      />
+    </template>
+  </div>
+</template>
+
+<script>
+import FullCalendar from "@fullcalendar/vue";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+// import calData from '../assets/calData'
+
+export default {
+  components: {
+    FullCalendar // make the <FullCalendar> tag available
+  },
+  data: () => {
+    return {
+      calendarPlugins: [
+        // plugins must be defined in the JS
+        dayGridPlugin,
+        timeGridPlugin,
+        interactionPlugin // needed for dateClick
+      ],
+      calendarWeekends: false,
+      calendarEvents: [
+        // initial event data
+        { title: "Event Now", start: new Date() }
+      ]
+    };
+  },
+  methods: {
+    toggleWeekends() {
+      this.calendarWeekends = !this.calendarWeekends; // update a property
+    },
+    gotoPast() {
+      let calendarApi = this.$refs.fullCalendar.getApi(); // from the ref="..."
+      calendarApi.gotoDate("2000-01-01"); // call a method on the Calendar object
+    },
+    handleDateClick(arg) {
+      if (confirm("Would you like to add an event to " + arg.dateStr + " ?")) {
+        this.calendarEvents.push({
+          // add new event data
+          title: "New Event",
+          start: arg.date,
+          allDay: arg.allDay
+        });
+      }
+    }
+  }
+};
+</script>
+<style lang='scss'>
+@import "~@fullcalendar/core/main.css";
+@import "~@fullcalendar/daygrid/main.css";
+@import '~@fullcalendar/timegrid/main.css';
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+// This import loads the firebase namespace.
+import firebase from 'firebase/app';
+ 
+// These imports load individual services into the firebase namespace.
+import 'firebase/auth';
+import 'firebase/firestore';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = true
+
+firebase.initializeApp({
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+});
+
+export const db = firebase.firestore();
 
 new Vue({
   render: h => h(App),
